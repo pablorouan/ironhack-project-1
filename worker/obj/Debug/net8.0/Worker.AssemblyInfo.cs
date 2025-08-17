c7881a4fdc788679c0df081c35f8c17af8fd70e8
pablorouan <p.rouans@gmail.com>
2025-08-17 13:09:37 +0200

    Adding a start-all.sh and stop-all.sh scripts to quickly run locally the app.

--- worker/obj/Debug/net8.0/Worker.AssemblyInfo.cs
+++ worker/obj/Debug/net8.0/Worker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Worker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f06b69544bbdc6ae3491a28b86cdbc8fbe9b3141")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c7e4e23a94fe5690631012ec02fc36e0181093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Worker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Worker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
